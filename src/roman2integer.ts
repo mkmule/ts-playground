9efeb073393ace268c48d90d55a5e0ed5d70c9ca
Serhiy <serhiy.mos@gmail.com>
2024-01-20 12:52:21 +0400

    Alternative solution assuming that "numbers are ALWAYS written largest to smallest from left to right"

--- src/roman2integer.ts
+++ src/roman2integer.ts
@@ -8,33 +8,38 @@ const dictionary = {
   M: 1000,
 };
 
-const subtractionRules = {
-  'I': ['V', 'X'],
-  'X': ['L', 'C'],
-  'C': ['D', 'M'],
-};
-const subtractableChars = Object.keys(subtractionRules);
+// const subtractionRules = {
+//   'I': ['V', 'X'],
+//   'X': ['L', 'C'],
+//   'C': ['D', 'M'],
+// };
+// const subtractableChars = Object.keys(subtractionRules);
+
+// export function romanToInt(s: string): number {
+//   let count = 0;
+//
+//   for (let i = 0; i < s.length; i++) {
+//     const currentChar = s.charAt(i);
+//
+//     if (subtractableChars.includes(currentChar)) {
+//       const nextChar = s.charAt(i + 1);
+//       const allowedRootSymbols = subtractionRules[currentChar] || [];
+//
+//       if (allowedRootSymbols.includes(nextChar)) {
+//         count += (dictionary[nextChar] - dictionary[currentChar]);
+//         i++;
+//       } else {
+//         count += dictionary[currentChar];
+//       }
+//     } else {
+//       count += dictionary[currentChar];
+//     }
+//   }
+//
+//   return count;
+// }
 
 export function romanToInt(s: string): number {
-  let count = 0;
-
-  for (let i = 0; i < s.length; i++) {
-    const currentChar = s.charAt(i);
-
-    if (subtractableChars.includes(currentChar)) {
-      const nextChar = s.charAt(i + 1);
-      const allowedRootSymbols = subtractionRules[currentChar] || [];
-
-      if (allowedRootSymbols.includes(nextChar)) {
-        count += (dictionary[nextChar] - dictionary[currentChar]);
-        i++;
-      } else {
-        count += dictionary[currentChar];
-      }
-    } else {
-      count += dictionary[currentChar];
-    }
-  }
-
-  return count;
+  const numbers = s.split('').map(char => dictionary[char]);
+  return numbers.reduce((prev, cur, i) => cur < numbers[i + 1] ? prev - cur : prev + cur, 0);
 }
