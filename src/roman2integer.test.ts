d52515a6cbde58c259196653b079c283d9dc4aa1
Serhiy <serhiy.mos@gmail.com>
2024-01-20 12:09:33 +0400

    Prepare sandbox with dynamic testing

--- src/roman2integer.test.ts
+++ src/roman2integer.test.ts
@@ -1,12 +1,14 @@
 import { romanToInt } from './roman2integer';
 
 describe('romanToInt: Convert roman numerals to integers', () => {
-  test(`converts 'III' to 3`, () => {
-    // Given
-    const inputRoman = 'III';
-    const outputInteger = 3;
+  const cases = [
+    ['III', 3],
+    ['LVIII', 58],
+    ['MCMXCIV', 1994],
+  ];
 
-    // Then
-    expect(romanToInt(inputRoman)).toBe(outputInteger);
+
+  test.each(cases)(`converts %p to %p`, (input: any, output) => {
+    expect(romanToInt(String(input))).toBe(output);
   });
 });
--- src/roman2integer.ts
+++ src/roman2integer.ts
@@ -1,3 +1,13 @@
+const dictionary = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
 export function romanToInt(s: string): number {
   console.log(s);
   return -1;
